66407665d9a69cbfaee3d4d1807c42858925758d
Vladlen8432 <127045075+Vladlen8432@users.noreply.github.com>
2023-10-28 13:37:53 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,66 @@
+const productData = [
+  {
+    id: '3',
+    title: 'Tacos with lime M',
+    price: 5.85,
+    discount: 15,
+  },
+
+  {
+    id: '2',
+    title: 'Tacos with lime XXL',
+    price: 10.99,
+    discount: 15,
+  },
+
+  {
+    id: '1',
+    title: 'Tacos with lime XL',
+    price: 6.99,
+    discount: null,
+  },
+];
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <section>
+      <h1>Hello WORLD🙂</h1>
+
+      {productData.map(product => {
+        return (
+          <Product
+            key={product.id}
+            title={product.title}
+            price={product.price}
+            discount={product.discount}
+          />
+        );
+      })}
+
+      {/* <Product title="Tacos with lime" price="10.99" discount={null} />
+      <Product title="Tacos L" price="7.66" discount={1.5} />
+      <Product title="Tacos S" price="4.55" discount={2.33} /> */}
+    </section>
+  );
+};
+
+const Product = ({ title, price, discount }) => {
+  return (
+    <div>
+      <img
+        src="https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640"
+        alt="TACOS WITH LIME"
+        width="640"
+      />
+
+      <h2>{title}</h2>
+      {discount ? (
+        <h3>Discount: {discount}$</h3>
+      ) : (
+        <p>Sorry, but discount on this type of product has expired!</p>
+      )}
+      <p>Price: {price}$</p>
+      <button type="button">Add to card</button>
     </div>
   );
 };
